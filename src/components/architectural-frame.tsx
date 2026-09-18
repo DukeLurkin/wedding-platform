9d72127f044f181f5ffdd36a2f703fa08c054474
DukeLurkin <durkin.lukas@gmail.com>
2026-09-18 00:17:30 -0400

    feat: style neoclassical architectural frame

--- src/components/architectural-frame.tsx
+++ src/components/architectural-frame.tsx
@@ -1,23 +1,33 @@
+import Image from "next/image";
+
 type ArchitecturalFrameProps = {
   children: React.ReactNode;
 };
 
-export function ArchitecturalFrame({
-  children,
-}: ArchitecturalFrameProps) {
+export function ArchitecturalFrame({ children }: ArchitecturalFrameProps) {
   return (
-    <div className="relative min-h-screen">
-      <div
+    <div className="relative min-h-screen overflow-x-hidden">
+      <Image
+        src="/images/architecture/marble-column.png"
+        alt=""
+        width={950}
+        height={1655}
+        priority
         aria-hidden="true"
-        className="pointer-events-none fixed inset-y-0 left-0 z-40 hidden w-20 border-r border-limestone bg-parchment lg:block xl:w-28"
+        className="pointer-events-none fixed left-0 top-1/2 z-40 hidden h-[107vh] w-auto max-w-none -translate-x-[40%] -translate-y-1/2 select-none lg:block"
       />
 
-      <div
+      <Image
+        src="/images/architecture/marble-column.png"
+        alt=""
+        width={950}
+        height={1655}
+        priority
         aria-hidden="true"
-        className="pointer-events-none fixed inset-y-0 right-0 z-40 hidden w-20 border-l border-limestone bg-parchment lg:block xl:w-28"
+        className="pointer-events-none fixed right-0 top-1/2 z-40 hidden h-[107vh] w-auto max-w-none translate-x-[40%] -translate-y-1/2 select-none lg:block"
       />
 
-      <div className="lg:px-20 xl:px-28">{children}</div>
+      <div className="lg:px-24 xl:px-32">{children}</div>
     </div>
   );
 }
